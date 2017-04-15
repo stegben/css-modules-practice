3d002f2d93ddb0cd2ea900dfa7ac5fe4e6787b74
cph <stegben.benjamin@gmail.com>
2017-04-15 21:11:53 +0800

    try styled-components

--- src/TitleB.js
+++ src/TitleB.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-import style from './TitleB.css';
+import styled from 'styled-components';
 
-class TitleB extends Component {
-  render() {
-    return (
-      <div className={style.title}>
-        {this.props.content}
-      </div>
-    );
-  }
-}
+const TitleB = styled.div`
+  font-size: ${props => props.number}px;
+  color: ${props => props.primary ? 'rgba(100, 200, 100, 0.9)' : 'rgba(200, 100, 0, 0.9)'};
+`;
 
 export default TitleB;
--- src/index.js
+++ src/index.js
@@ -6,11 +6,29 @@ import TitleB from './TitleB';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      primary: true,
+      number: 10,
+    };
+  }
+
+  changePrimary() {
+    const { primary, number } = this.state;
+    this.setState({
+      primary: !primary,
+      number: (number + 100),
+    });
+  }
+
   render() {
+    const { number, primary } = this.state;
     return (
       <div>
+        <button onClick={this.changePrimary.bind(this)}></button>
         <TitleA content="Hello" />
-        <TitleB content="World" />
+        <TitleB primary={primary} number={number}>Worlds</TitleB>
       </div>
     );
   }
